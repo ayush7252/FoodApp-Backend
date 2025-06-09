2d09d51f5d0f76f0abc858024a7f80d0a2b9406d
Ayush Kumar Singh <ayush.singh@magicsw.com>
2025-06-09 15:18:20 +0530

    update code

--- Config/db.js
+++ Config/db.js
@@ -3,7 +3,11 @@ mongoose.set("strictQuery", true);
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 10000,
+});
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
