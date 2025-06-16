96eadd124c32050015cd7f8fe030d96ef3ce0e0b
Ayush Kumar Singh <ayush.singh@magicsw.com>
2025-06-16 14:54:51 +0530

    update code

--- Routes/NotificationRoutes.js
+++ Routes/NotificationRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer"); // Added multer import
 const {
   createSellerNotification,
   getAllNotifications,
